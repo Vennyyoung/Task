b871de90f7b33cbbb385e55fd334877aaa786a51
Vennyyoung <57029756+Vennyyoung@users.noreply.github.com>
2020-03-26 18:47:23 +0100

    Add files via upload

---
+++ jsTask.js
@@ -0,0 +1,15 @@
+let myName = "Nwaribeaku Venitus Chigozie";
+let myCourses = ["HTML", "CSS", "JS", "C#"] ;
+var numOfCourses = courses.length;
+let numDisplay = "";
+for (let i = 1; i <= 200; i++){
+    if (numberOfCourses % 2 == 1){
+        if (i % 2 == 1){
+            if(i == 200 - 1){
+                break;   
+            }
+            numDisplay +=i+2+"";
+        }    
+    }
+}
+console.log(numDisplay)
