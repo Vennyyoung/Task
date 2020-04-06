697ac9b689c526a8a64abe9bb8eae4f097502c21
Vennyyoung <57029756+Vennyyoung@users.noreply.github.com>
2020-04-07 00:51:06 +0100

    Update jsTask.js

--- jsTask.js
+++ jsTask.js
@@ -1,6 +1,6 @@
 let myName = "Nwaribeaku Venitus Chigozie";
 let myCourses = ["HTML", "CSS", "JS", "C#"] ;
-var numOfCourses = courses.length;
+var numOfCourses = mycourses.length;
 let numDisplay = "";
 for (let i = 1; i <= 200; i++){
     if (numberOfCourses % 2 == 1){
